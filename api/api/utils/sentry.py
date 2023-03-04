2eaf014f70e106629b1fde6be63ba9103972469e
Kodey Converse <kodey@conve.rs>
2023-03-04 10:37:45 -0500

    Try to fix the Sentry integration
    Signed-off-by: Kodey Converse <kodey@conve.rs>

--- api/api/utils/sentry.py
+++ api/api/utils/sentry.py
@@ -1,9 +1,12 @@
+import logging
 import os
+
 import sentry_sdk
 
 
 def init():
     if os.environ.get("IS_DEPLOYED", "false") == "true":
+        logging.info("Initializing Sentry...")
         sentry_sdk.init(
             "https://25e54bfbb12348c39102a72199f10de4@o4504710244139008.ingest.sentry.io/4504710247874560",
             traces_sample_rate=1.0,
@@ -11,3 +14,5 @@ def init():
                 "profiles_sample_rate": 1.0,
             },
         )
+    else:
+        logging.warn("Disabling Sentry because app is not deployed")
